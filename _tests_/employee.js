b59bb0b9ab455f0a0b407e402fef328a61b9e964
idakukimiya <kukimiya.ida@gmail.com>
2022-08-15 11:32:32 -0400

    cleaning up code

--- _tests_/employee.js
+++ _tests_/employee.js
@@ -1,5 +1,3 @@
-nes (40 sloc)  1.47 KB
-
 const Employee = require("../lib/Employee");
 
 test("Can create an new employee.", () => {
@@ -8,45 +6,45 @@ test("Can create an new employee.", () => {
 })
 
 test("Testing name.", () => {
-    const name = "James";
+    const name = "Sammy";
     const employeeInstance = new Employee(name);
     expect(employeeInstance.name).toBe(name);
 })
 
 test("Testing ID.", () => {
     const id = 2;
-    const employeeInstance = new Employee("James", id);
+    const employeeInstance = new Employee("Sammy", id);
     expect(employeeInstance.id).toBe(id);
 })
 
 test("Testing email.", () => {
-    const email = "jamesljenks@gmail.com";
-    const employeeInstance = new Employee("James", 2, email);
+    const email = "sammylinks@gmail.com";
+    const employeeInstance = new Employee("Sammy", 2, email);
     expect(employeeInstance.email).toBe(email);
 })
 
 
 
 test("Gets name through getName method.", () => {
-    const testName = "James";
+    const testName = "Sammy";
     const employeeInstance = new Employee(testName);
     expect(employeeInstance.getName()).toBe(testName);
 })
 
 test("Can test ID through getID method.", () => {
     const testID = 2;
-    const employeeInstance = new Employee("James", testID);
+    const employeeInstance = new Employee("Sammy", testID);
     expect(employeeInstance.getId()).toBe(testID);
 })
 
 test("Can test email through getEmail method.", () => {
-    const testEmail = "jamesljenks@gmail.com";
-    const employeeInstance = new Employee("James", 2, testEmail);
+    const testEmail = "sammylinks@gmail.com";
+    const employeeInstance = new Employee("Sammy", 2, testEmail);
     expect(employeeInstance.getEmail()).toBe(testEmail);
 })
 
 test("Testing role.", () => {
     const returnValue = "Employee";
-    const employeeInstance = new Employee("James", 2, "jamesljenks@gmail.com");
+    const employeeInstance = new Employee("James", 2, "sammylinks@gmail.com");
     expect(employeeInstance.getRole()).toBe(returnValue);
 })
--- _tests_/intern.js
+++ _tests_/intern.js
@@ -3,18 +3,18 @@ const Intern = require("../lib/Intern");
 
 test("Can create school.", () => {
     const testSchool = "School Name";
-    const employeeInstance = new Intern("James", 2, "jamesljenks@gmail.com", testSchool);
+    const employeeInstance = new Intern("Sammy", 2, "sammylinks@gmail.com", testSchool);
     expect(employeeInstance.school).toBe(testSchool);
 });
 
 test("Testing officeNumber will return office number.", () => {
     const testSchool = "School Name";
-    const employeeInstance = new Intern("James", 2, "jamesljenks@gmail.com", testSchool);
+    const employeeInstance = new Intern("Sammy", 2, "sammylinks@gmail.com", testSchool);
     expect(employeeInstance.getSchool()).toBe(testSchool);
 });
 
 test("Testing role.", () => {
     const returnValue = "Intern";
-    const employeeInstance = new Intern("James", 2, "jamesljenks@gmail.com", "School Name");
+    const employeeInstance = new Intern("Sammy", 2, "sammylinks@gmail.com", "School Name");
     expect(employeeInstance.getRole()).toBe(returnValue);
 });
